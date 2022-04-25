0bf5a94b0369853751aa41c7190c921d6256b964
Wils100 <98708199+Wils100@users.noreply.github.com>
2022-04-25 12:52:32 +0100

    Add files via upload

--- 80sQUIZJS.js
+++ 80sQUIZJS.js
@@ -1,5 +1,26 @@
-<!80sQUIZ.js>
+//80sQUIZJS.js
 
-function helloFunction () {
-							alert (’Hello Napier ’);
-						}
+if(document.getElementById('summer').checked) {   
+       var selectedValue = document.getElementById('summer').value;  
+       alert("Selected Radio Button is: " + selectedValue);    
+}  
+
+document.querySelector('input[name="Bohemian"]:checked')  
+
+function checkButton() {    
+            if(document.getElementById('Bohemian').checked) {   
+                document.getElementById("disp").innerHTML   
+                    = document.getElementById("correct").value   
+                    + " radio button is checked";   
+            }    
+			
+			var score=0;
+function check (name) {
+   var methods = document.getElementsByName(name);
+    for (var i=0; i<methods.length; i++) {
+         if (methods[i].checked == true) {
+             score +=1;
+             alert(score);
+            }
+   }
+}
