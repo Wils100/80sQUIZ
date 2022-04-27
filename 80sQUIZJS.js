b9d974c8a81d993a808f99f98553f97020eefb28
Wils100 <98708199+Wils100@users.noreply.github.com>
2022-04-27 13:55:39 +0100

    Add files via upload

--- 80sQUIZJS.js
+++ 80sQUIZJS.js
@@ -1,20 +1,31 @@
 
-        function displayScore() {
+        function displayScore() 
+		{
             document.getElementById("total").innerHTML = "";
             var ele = document.getElementsByTagName('input');
-              
-            for(i = 0; i < ele.length; i++) {
+			              
+            for(i = 0; i < ele.length; i++) 
+			{
                   
-                if(ele[i].type="radio") {
+                if(ele[i].type="radio") 
+				{
                   
                     if(ele[i].checked)
                         document.getElementById("total").innerHTML
                                 += ele[i].name + " Your Score: "
                                 + ele[i].value + "<br>";
+				}
 								
-                }
             }
+				
         }
+		
+		function refreshpage()
+		{
+			window.location.href=
+			"http://wils100.github.io/80sQUIZ/";
+		}
+			
  
 
 
